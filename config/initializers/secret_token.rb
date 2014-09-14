264eeadc9b2b65b75204075ac20def60ea36b28e
HWG <hwgilling@gmail.com>
2014-09-13 18:21:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,40 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+# SampleApp4::Application.config.secret_key_base = 'f30ff6b417a0935e7c67d7b54d1b280b7eb9da0486b97f317b52b6c48ad67426d4cdbadb0db609fcea8ecaba92b7cc73027e8d5c82687e32c9f3dea2f73a2068'
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp4::Application.config.secret_key_base = secure_token
